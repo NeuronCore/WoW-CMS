8a68d670e1db8e426c485da8842c80981884acc6
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2023-07-11 08:29:08 +0330

    feat(Server/Core): Account - GraphQL

--- server/src/account/account.module.ts
+++ server/src/account/account.module.ts
@@ -4,12 +4,13 @@ import { DatabaseModule } from '@/database/database.module';
 
 import { AccountService } from './account.service';
 import { AccountController } from './account.controller';
+import { AccountResolver } from '@/account/account.resolver';
 
 @Module
 ({
     imports: [DatabaseModule],
     controllers: [AccountController],
-    providers: [AccountService]
+    providers: [AccountService, AccountResolver]
 })
 export class AccountModule
 {
--- server/src/account/dto/update-password.dto.ts
+++ server/src/account/dto/update-password.dto.ts
@@ -1,21 +1,26 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
+@InputType()
 export class UpdatePasswordDto
 {
     @ApiProperty()
     @IsNotEmpty()
+    @Field()
     public readonly currentPassword: string;
 
     @ApiProperty()
     @IsNotEmpty()
     @MinLength(5)
     @MaxLength(32)
+    @Field()
     public readonly newPassword: string;
 
     @ApiProperty()
     @IsNotEmpty()
     @MinLength(5)
     @MaxLength(32)
+    @Field()
     public readonly newPasswordConfirm: string;
 }
