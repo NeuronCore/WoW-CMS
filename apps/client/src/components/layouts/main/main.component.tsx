2306ec58f5bf4291cf7dcb40f1135dd78f9c90e2
im-parsa <parsa.firoozi55@gmail.com>
2023-07-23 23:02:55 +0330

    feat(Client/Core): Account Overview P1

--- apps/client/src/components/layouts/main/main.component.tsx
+++ apps/client/src/components/layouts/main/main.component.tsx
@@ -20,7 +20,7 @@ const Preloader = dynamic(() => import('@/components/preloader'));
 const Navbar = dynamic(() => import('@/components/layouts/navbar/navbar.component'));
 const Footer = dynamic(() => import('@/components/layouts/footer/footer.component'));
 
-import { aside } from '@/data/account.data.json';
+import { aside as asideData } from '@/data/account.data.json';
 
 interface Props
 {
@@ -66,7 +66,7 @@ const Main = ({ children }: Props) =>
 
                                     <ul className={stylesAccount.accountAsideList}>
                                         {
-                                            aside.map((item, index) =>
+                                            asideData.map((item, index) =>
                                                 (
                                                     <Fragment key={createUniqueKey([item.title, index])}>
                                                         <li className={stylesAccount.accountAsideListItemTitle}>
--- apps/client/src/pages/account/overview.tsx
+++ apps/client/src/pages/account/overview.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import dynamic from 'next/dynamic';
+
+import { BsDiscord, BsPen } from 'react-icons/bs';
 
 import styles from '@/styles/pages/account.module.scss';
 
 import { useUser } from '@/hooks/use-user';
+
+import Logo from '@/components/logo';
+
+const Button = dynamic(() => import('@/components/button'));
 
 const Overview = () =>
 {
@@ -10,7 +19,108 @@ const Overview = () =>
 
     return (
         <div className={styles.accountContent}>
-            {user.username}
+            <div className={styles.accountContentList}>
+                <div className={styles.accountContentItem}>
+                    <header>
+                        <h3>
+                            Your Information
+                        </h3>
+
+                        <Link href='/account/details'>
+                            <BsPen /> Edit
+                        </Link>
+                    </header>
+                    <div>
+                        <span>
+                            <Image
+                                src={ Logo() }
+                                alt='WoW CMS'
+                                fill
+                                style={{ objectFit: 'contain' }}
+                                sizes={'100'}
+                            />
+                        </span>
+                        <ul>
+                            <li>
+                                <p>
+                                    First Name:
+                                </p>
+                                <span>
+                                    { user.first_name }
+                                </span>
+                            </li>
+                            <li>
+                                <p>
+                                    Last Name:
+                                </p>
+                                <span>
+                                    { user.last_name }
+                                </span>
+                            </li>
+                            <li>
+                                <p>
+                                    Username:
+                                </p>
+                                <span>
+                                    { user.username }
+                                </span>
+                            </li>
+                            <li>
+                                <p>
+                                    Email:
+                                </p>
+                                <span>
+                                    { user.email }
+                                </span>
+                            </li>
+                            <li>
+                                <p>
+                                    Discord:
+                                </p>
+                                {
+                                    user.discord
+                                        ?
+                                        <span>
+                                            hello_im_parsa
+                                        </span>
+                                        :
+                                        <Button>
+                                            <BsDiscord />
+                                            Add Discord Account
+                                        </Button>
+                                }
+                            </li>
+                            {
+                                user.phone
+                                    ?
+                                    <li>
+                                        <p>
+                                            Phone:
+                                        </p>
+                                        <span>
+                                            { user.phone }
+                                        </span>
+                                    </li>
+                                    : null
+                            }
+                        </ul>
+                    </div>
+                </div>
+                <div className={styles.accountContentItem}>
+                    <header>
+                        <h3>
+                            ACCOUNT SECURITY
+                        </h3>
+
+                        <Link href='/account/security'>
+                            <BsPen /> Edit
+                        </Link>
+                    </header>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
