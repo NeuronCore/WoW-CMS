85c957eafdb0f4769818ed7e243c98f526bb5be5
im-parsa <parsa.firoozi55@gmail.com>
2023-07-23 17:33:20 +0330

    feat(Client/Core): Account Base System P1

--- apps/client/src/components/layouts/main/main.component.tsx
+++ apps/client/src/components/layouts/main/main.component.tsx
@@ -1,16 +1,25 @@
+import Link from 'next/link';
 import dynamic from 'next/dynamic';
+import classnames from 'classnames';
 import { useRouter } from 'next/router';
-import React, { ReactNode, useEffect } from 'react';
+import React, { Fragment, ReactNode, useEffect } from 'react';
 
 import styles from '@/components/layouts/main/main.module.scss';
+import stylesAccount from '@/styles/pages/account.module.scss';
 
 import { routesData } from '@/data/routes.data';
 
+import { useUser } from '@/hooks/use-user';
 import { useAppDispatch } from '@/redux/app/hooks';
+
 import { setTheme } from '@/redux/features/environment/environment-slice';
+
+import { createUniqueKey } from '@/utils/helper.util';
 
 const Navbar = dynamic(() => import('@/components/layouts/navbar/navbar.component'));
 const Footer = dynamic(() => import('@/components/layouts/footer/footer.component'));
+
+import { aside } from '@/data/account.data.json';
 
 interface Props
 {
@@ -19,7 +28,8 @@ interface Props
 
 const Main = ({ children }: Props) =>
 {
-    const { pathname } = useRouter();
+    const [user] = useUser();
+    const { push, pathname } = useRouter();
     const dispatch = useAppDispatch();
 
     useEffect(() =>
@@ -27,16 +37,64 @@ const Main = ({ children }: Props) =>
         dispatch(setTheme(process.env.NEXT_PUBLIC_THEME));
     }, []);
 
+    useEffect(() =>
+    {
+        (
+            async() =>
+            {
+                if (!user && pathname.split('/')[1] === 'account')
+                    await push('/login');
+            }
+        )();
+    }, [user, pathname]);
+
     return (
         <>
             <Navbar />
 
             <div className={styles.main}>
-                { children }
+                {
+                    pathname.split('/')[1] === 'account'
+                        ?
+                        <section className={stylesAccount.accountContainer}>
+                            <aside className={stylesAccount.accountAside}>
+                                <h1 className={stylesAccount.accountAsideHeader}>Settings</h1>
+
+                                <ul className={stylesAccount.accountAsideList}>
+                                    {
+                                        aside.map((item, index) =>
+                                            (
+                                                <Fragment key={createUniqueKey([item.title, index])}>
+                                                    <li className={stylesAccount.accountAsideListItemTitle}>
+                                                        { item.title }
+                                                    </li>
+                                                    {
+                                                        item.items.map((link, index) =>
+                                                            (
+                                                                <li key={createUniqueKey([item.title, link.title, index])} className={classnames(stylesAccount.accountAsideListItem, { [stylesAccount.accountAsideListItemActive]: pathname.includes(link.path) })}>
+                                                                    <Link href={link.path}>
+                                                                        { link.title }
+                                                                    </Link>
+                                                                </li>
+                                                            ))
+                                                    }
+                                                </Fragment>
+                                            ))
+                                    }
+                                    <li className={classnames(stylesAccount.accountAsideListItem, stylesAccount.accountAsideListItemLogout)}>
+                                        Log Out
+                                    </li>
+                                </ul>
+                            </aside>
+                            { children }
+                        </section>
+                        :
+                        children
+                }
             </div>
 
             {
-                routesData.noFooterRoutes.includes(pathname)
+                routesData.noFooterRoutes.includes(pathname.split('/')[1])
                     ? null
                     : <Footer />
             }
--- apps/client/src/pages/account/index.tsx
+++ apps/client/src/pages/account/index.tsx
@@ -1,15 +1,9 @@
-import Link from 'next/link';
-import classnames from 'classnames';
 import { useRouter } from 'next/router';
-import React, { useEffect, Fragment } from 'react';
-
-import { aside } from '@/data/account.data.json';
+import React, { useEffect } from 'react';
 
 import styles from '@/styles/pages/account.module.scss';
 
 import { useUser } from '@/hooks/use-user';
-
-import { createUniqueKey } from '@/utils/helper.util';
 
 const Account = () =>
 {
@@ -23,50 +17,16 @@ const Account = () =>
             {
                 if (!user)
                     await push('/login');
-                // else if (pathname == '/account')
-                //     await push('/account/overview');
+                else if (pathname == '/account')
+                    await push('/account/overview');
             }
         )();
     }, [user]);
 
     return (
-        <main className={styles.account}>
-            <section className={styles.accountContainer}>
-                <aside className={styles.accountAside}>
-                    <h1 className={styles.accountAsideHeader}>Settings</h1>
+        <div className={styles.accountContent}>
 
-                    <ul className={styles.accountAsideList}>
-                        {
-                            aside.map((item, index) =>
-                                (
-                                    <Fragment key={createUniqueKey([item.title, index])}>
-                                        <li className={styles.accountAsideListItemTitle}>
-                                            { item.title }
-                                        </li>
-                                        {
-                                            item.items.map((link, index) =>
-                                                (
-                                                    <li key={createUniqueKey([item.title, link.title, index])} className={classnames(styles.accountAsideListItem, { [styles.accountAsideListItemActive]: pathname.includes(link.path) })}>
-                                                        <Link href={link.path}>
-                                                            { link.title }
-                                                        </Link>
-                                                    </li>
-                                                ))
-                                        }
-                                    </Fragment>
-                                ))
-                        }
-                        <li className={classnames(styles.accountAsideListItem, styles.accountAsideListItemLogout)}>
-                            Log Out
-                        </li>
-                    </ul>
-                </aside>
-
-                <div className={styles.accountContent}>
-
-                </div>
-            </section>
-        </main>
+        </div>
     );
 };
 
