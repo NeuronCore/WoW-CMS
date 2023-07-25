d6498c80c10dd2c2a96964c2a6f11fa85255f4d4
im-parsa <parsa.firoozi55@gmail.com>
2023-07-25 14:25:11 +0330

    feat(Client/Core): Navbar Locale Popup

--- apps/client/src/components/layouts/navbar/navbar.component.tsx
+++ apps/client/src/components/layouts/navbar/navbar.component.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import { v4 as uuidV4 } from 'uuid';
-import React, {useEffect, useRef, useState} from 'react';
 import { useRouter } from 'next/router';
+import React, { useRef, useState } from 'react';
 import { animated, useSpring } from 'react-spring';
 import { BsPerson, BsList, BsCoin } from 'react-icons/bs';
 
