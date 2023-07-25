f1a9a27260e0e17c33b482002297cb9abd8fd5cd
im-parsa <parsa.firoozi55@gmail.com>
2023-07-25 12:36:10 +0330

    feat(Client/Core): Navbar Locale System

--- apps/client/src/components/layouts/navbar/navbar.component.tsx
+++ apps/client/src/components/layouts/navbar/navbar.component.tsx
@@ -2,8 +2,10 @@ import Link from 'next/link';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import { v4 as uuidV4 } from 'uuid';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import { BsPerson, BsList } from 'react-icons/bs';
+import { animated, useSpring } from 'react-spring';
+import { BsPerson, BsList, BsCoin } from 'react-icons/bs';
 
 import Logo from '@/components/logo';
 
@@ -18,7 +20,11 @@ import { useUser } from '@/hooks/use-user';
 const Navbar = () =>
 {
     const [user] = useUser();
-    const { pathname } = useRouter();
+    const { pathname, asPath, locale, locales } = useRouter();
+
+    const [localePopup, setLocalePopup] = useState(false);
+
+    const springStyles = useSpring({ opacity: localePopup ? 1 : 0 });
 
     return (
         <nav className={styles.navbar} data-secondery={ pathname.includes('blogs/') }>
@@ -44,17 +50,50 @@ const Navbar = () =>
                         ))
                     }
                 </ul>
-                {
-                    user
-                        ?
-                        <Button href='/account'>
-                            Account
-                        </Button>
-                        :
-                        <Button href='/login'>
-                            Login/Register
-                        </Button>
-                }
+                <div>
+                    {
+                        locale && locales
+                            ?
+                            <div onClick={() => setLocalePopup(!localePopup)}>
+                                <p>
+                                    EN
+                                </p>
+                                {
+                                    localePopup
+                                        ?
+                                        <animated.div style={ springStyles }>
+                                            {
+                                                locales.map((localeData) =>
+                                                    (
+                                                        <Link key={localeData} href={asPath} locale={localeData} data-active={locale.toLocaleUpperCase() === localeData.toLocaleUpperCase()}>
+                                                            { localeData.toUpperCase() }
+                                                        </Link>
+                                                    ))
+                                            }
+                                        </animated.div>
+                                        : null
+                                }
+                            </div>
+                            : null
+                    }
+                    {
+                        user
+                            ?
+                            <>
+                                <span>
+                                    <BsCoin />
+                                    { user.coins ?? 0 }
+                                </span>
+                                <Button href='/account'>
+                                    Account
+                                </Button>
+                            </>
+                            :
+                            <Button href='/login'>
+                                Login/Register
+                            </Button>
+                    }
+                </div>
             </div>
 
             <div className={styles.navbarContainerMobile}>
