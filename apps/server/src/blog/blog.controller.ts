f2f438ff39b652232bfd252a9986a9d7a0efbd41
Nooshin Moqanian <59511052+nooshinmoqanian@users.noreply.github.com>
2023-08-06 10:15:24 +0330

    fix(server/content):Blog

--- apps/server/src/blog/blog.controller.ts
+++ apps/server/src/blog/blog.controller.ts
@@ -126,7 +126,7 @@ export class BlogController
         return this.blogService.findAllAndOrder(locale, type, page, limit);
     }
 
-    @Get('/search-In-Content-And-Summary')
+    @Get('/search-in-content-and-summary')
     public async searchInContentAndSummary(@Query('locale') locale: Locale, @Query('search') search: string, @Query('page') page: number, @Query('limit') limit: number)
     {
         return this.blogService.searchInContentAndSummary(locale, search, page, limit);
